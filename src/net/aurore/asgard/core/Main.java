637e75074183bcfd3cb3540367c4b34ebbbd32c2
Pierre Grenier <pierre.grenier@etudiant.univ-lille1.fr>
2019-02-06 15:51:27 +0100

    Initial commit

---
+++ src/net/aurore/asgard/core/Main.java
@@ -0,0 +1,24 @@
+package net.aurore.asgard.core;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application{
+	
+	public Main() {
+		
+	}
+	
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		Application.launch(Main.class, args);
+	}
+
+
+}
